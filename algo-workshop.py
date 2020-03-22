5231d32e708d4996a7b1716f9ed85189dd6249e4
lukasz lesiuk <lukasz.lesiuk.2@gmail.com>
2020-03-22 12:29:15 +0100

    first funcion

--- algo-workshop.py
+++ algo-workshop.py
@@ -3,7 +3,20 @@ def get_even_numbers(x, stop, z):
     Returns a list containing first 'x' even elements lower than 'stop'.
     Those elements must be divisible by 'z'. 
     """
-    pass
+    raw_list = []
+    for i in range(1,(stop//2)):
+        raw_list.append(2*i)
+    # print(raw_list)
+
+    list_sorted = []
+    for element in raw_list:
+        if element%z != 0:
+            list_sorted.append(element)
+    # print(list_sorted)
+
+    for i in range(x):
+        print(list_sorted[i])
+
 
 
 def get_sum_of_greatest_elements(my_list, x):
@@ -12,3 +25,5 @@ def get_sum_of_greatest_elements(my_list, x):
     i.e. Returns a sum of 3 biggest elements from [2, 18, 5, -11, 7, 6, 9]
     """
     pass
+
+get_even_numbers(2, 15, 10)
