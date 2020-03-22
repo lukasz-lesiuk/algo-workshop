758e55909c78c0c2a3c6e80c42d79d2b55d18981
lukasz lesiuk <lukasz.lesiuk.2@gmail.com>
2020-03-22 11:29:00 +0100

    Initiated functions pototype

---
+++ algo-workshop.py
@@ -0,0 +1,14 @@
+def get_even_numbers(x, stop, z):
+    """
+    Returns a list containing first 'x' even elements lower than 'stop'.
+    Those elements must be divisible by 'z'. 
+    """
+    pass
+
+
+def get_sum_of_greatest_elements(my_list, x):
+    """
+    Returns a single integer, which is a sum of 'x' biggest elements from 'my_list'
+    i.e. Returns a sum of 3 biggest elements from [2, 18, 5, -11, 7, 6, 9]
+    """
+    pass
